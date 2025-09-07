035588400ee7fbc57d09cc33cb55fe74c10f01d4
hodewdew-creator <moyadais@gmail.com>
2025-09-07 20:54:29 +0900

    Update SuggestTemplateModal.jsx

--- src/components/SuggestTemplateModal.jsx
+++ src/components/SuggestTemplateModal.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useMemo, useState } from "react";
-import templates from "./data/templates.json";
 import SuggestTemplateModal from "./components/SuggestTemplateModal";
 
 /** 
